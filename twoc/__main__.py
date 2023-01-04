efbbb26feec3e9318dc98a7afb6e89dfc22a2a74
Will Bettridge-Radford <william.bettridge.radford@gmail.com>
2023-01-04 22:29:16 +0000

    add argument to run the code using tcc

--- twoc/__main__.py
+++ twoc/__main__.py
@@ -1,27 +1,52 @@
 import argparse
+
+from subprocess import Popen, PIPE
 from sys import argv
 from unwind import unwind_file
 from . import *
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-o", "--outfile")
-parser.add_argument("file", nargs='+')
-args = parser.parse_args()
+def run(code):
+    process = Popen(
+            ['tcc', '-run', '-', '-Wall'],
+            stdin=PIPE,
+            stdout=PIPE)
 
-cu = CompilationUnit()
+    input_bytes = code.encode()
+    outs, errs = process.communicate(input=input_bytes, timeout=15)
+    output = outs.decode()
 
-for file in args.file:
-    cu.read_file(file)
+    print(output)
 
-if args.outfile == None:
-    cu.print()
-else:
-    lines = cu.render()
 
-    out_path = Path(args.outfile)
-    if not out_path.parent.exists():
-        raise FileNotFoundError(f"parent folder doesn't exist: {out_path.parent}")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--outfile")
+    parser.add_argument("-run", help="run compiled source", action="store_true")
+    parser.add_argument("file", nargs='+')
+    args = parser.parse_args()
 
-    with open(args.outfile, 'w') as f:
-        f.write(lines)
+    cu = CompilationUnit()
+
+    for file in args.file:
+        cu.read_file(file)
+
+    code = cu.render()
+
+    if args.run:
+        run(code)
+        return
+
+    if args.outfile == None:
+        print(code)
+    else:
+
+        out_path = Path(args.outfile)
+        if not out_path.parent.exists():
+            raise FileNotFoundError(f"parent folder doesn't exist: {out_path.parent}")
+
+        with open(args.outfile, 'w') as f:
+            f.write(code)
+
+if __name__ == '__main__':
+    main()
 
