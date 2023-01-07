67fafdd08be6ac663d7ae6320cfdf02ce7c62f2b
William Bettridge-Radford <william.bettridge.radford@gail.com>
2023-01-07 13:57:08 +0000

    remove transform_infix

--- twoc/__init__.py
+++ twoc/__init__.py
@@ -42,55 +42,6 @@ def compile_var(args, body):
         assert False
 
 
-def split_on_symbol(lst, s):
-    r = [[]]
-    for e in lst:
-        if e == s:
-            r.append([])
-        else:
-            r[-1].append(e)
-    return r
-
-
-def transform_infix(x):
-    if ',' in x:
-        sections = split_on_symbol(x, ',')
-    else:
-        sections = [x]
-
-    transformed_sections = []
-    for section in sections:
-        n = len(section)
-
-        if n == 0:
-            transformed_sections.append(section)
-            continue
-        elif n == 1:
-            transformed_sections.append(section[0])
-            continue
-        elif (n % 2) == 0:
-            print(x)
-            assert False
-
-        first_arg, first_op, *rest = x
-        xx = [first_op, first_arg]
-        for i in range(2, len(x)):
-            if i % 2:
-                assert first_op == x[i]
-            else:
-                xx.append(x[i])
-        return xx
-
-    n = len(transformed_sections)
-
-    if n == 0:
-        assert False
-    elif n == 1:
-        return transformed_sections[0]
-    else:
-        return transformed_sections
-
-
 def is_string_literal(s):
     if len(s) < 2:
         return False
