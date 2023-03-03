65aa2f5d692003aed0d48e0986a293d76788fee0
William Bettridge-Radford <william.bettridge.radford@gail.com>
2023-03-03 08:53:16 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='twoc',
+      version='0.1',
+      description='converts python syntax into a c',
+      author='William Bettridge-Radford',
+      author_email='william.bettridge.radford@gmail.com',
+      )
+
